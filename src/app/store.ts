f23b27bb4cf86ec2143b05a209821162f15b2ca0
xtansy <xtansy@mail.ru>
2023-10-17 11:15:01 +0300

    remove dev mode feature

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,50 +1,46 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {
-	persistStore,
-	persistReducer,
-	FLUSH,
-	REHYDRATE,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./rootReducer";
 
 const persistConfig = {
-	key: "root",
-	version: 1,
-	storage,
+    key: "root",
+    version: 1,
+    storage,
 };
 
 export const store = configureStore({
-	reducer: persistReducer(
-		persistConfig,
-		rootReducer
-	) as unknown as typeof rootReducer,
-	// ↑ cuz persistReducer broke infering RootState
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [
-					FLUSH,
-					REHYDRATE,
-					PAUSE,
-					PERSIST,
-					PURGE,
-					REGISTER,
-				],
-			},
-		}),
+    reducer: persistReducer(
+        persistConfig,
+        rootReducer
+    ) as unknown as typeof rootReducer,
+    // ↑ cuz persistReducer broke infering RootState
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 });
 
 export const persistedStore = persistStore(store);
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-window.store = store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
